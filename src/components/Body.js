ee549068b3b825842e8f822010ad9f71f1a2dd7b
nishit <nishitranjan41@gmail.com>
2024-07-15 21:20:00 +0530

    Filtered List with use state

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,37 +1,47 @@
 import RestaurantCard from "./RestaurantCard";
 import restaurants from "../utils/mockData";
+import { useState } from "react";
 
-const Body = () => (
-    <div
-      style={{
-        display: "flex",
-        flexWrap: "wrap",
-        justifyContent: "space-between",
-      }}
-    >
-      <RestaurantCard
-        src={
-          "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5dff22bc484599ce6f02501c73eb0f16"
-        }
-        resName={restaurants[0].info?.name}
-        cuisines={restaurants[0].info?.cuisines}
-        NoOfStar={restaurants[0].info?.avgRating}
-        timeTaken={restaurants[0].info?.sla?.slaString}
-      />
-  
-      {restaurants.map((restaurant) => (
-        <RestaurantCard
-          key={restaurant.info?.id}
-          src={
-            "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5dff22bc484599ce6f02501c73eb0f16"
-          }
-          resName={restaurant.info?.name}
-          cuisines={restaurant.info?.cuisines}
-          NoOfStar={restaurant.info?.avgRating}
-          timeTaken={restaurant.info?.sla?.slaString}
-        />
-      ))}
-    </div>
+const Body = () => {
+  const [filteredRestaurantsList, setFilteredRestaurantList] =
+    useState(restaurants);
+
+    console.log({filteredRestaurantsList});
+
+  return (
+    <>
+      <button
+        onClick={() => {
+          const filteredList = filteredRestaurantsList.filter(
+            (restaurant) => restaurant.info.avgRating > 4.1
+          );
+          setFilteredRestaurantList(filteredList);
+        }}
+      >
+        Filter
+      </button>
+      <div
+        style={{
+          display: "flex",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+        }}
+      >
+        {filteredRestaurantsList.map((restaurant) => (
+          <RestaurantCard
+            key={restaurant.info?.id}
+            src={
+              "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/5dff22bc484599ce6f02501c73eb0f16"
+            }
+            resName={restaurant.info?.name}
+            cuisines={restaurant.info?.cuisines}
+            NoOfStar={restaurant.info?.avgRating}
+            timeTaken={restaurant.info?.sla?.slaString}
+          />
+        ))}
+      </div>
+    </>
   );
+};
 
-  export default Body;
+export default Body;
