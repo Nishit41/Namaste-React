feb2f4c0365f8c443c3ad81a72d2dec8676b392a
nishit <nishitranjan41@gmail.com>
2024-07-20 15:51:06 +0530

    cors plugin installation form cors Proxy.io replaces cors extension

--- src/components/Body.js
+++ src/components/Body.js
@@ -13,7 +13,7 @@ const Body = () => {
   const [searchText, setSearchText] = useState(filteredRestaurantsList);
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.34260&lng=85.30990&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+      "https://corsproxy.io/https://www.swiggy.com/dapi/restaurants/list/v5?lat=23.34260&lng=85.30990&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     console.log(json?.data?.cards[4]);
